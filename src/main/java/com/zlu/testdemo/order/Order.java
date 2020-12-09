0546f466238f10a96c10def9543b9df830d72ea9
zlu <luzhaonwu@163.com>
2020-12-09 22:30:44 +0800

    add order model

---
+++ src/main/java/com/zlu/testdemo/order/Order.java
@@ -0,0 +1,11 @@
+package java.com.zlu.testdemo.order;
+
+
+@Builder
+@Getter
+public class Order {
+
+    private String id;
+
+    private String address;
+}
---
+++ src/test/java/com/zlu/testdemo/order/OrderTest.java
@@ -0,0 +1,10 @@
+package java.com.zlu.testdemo.order;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+}
